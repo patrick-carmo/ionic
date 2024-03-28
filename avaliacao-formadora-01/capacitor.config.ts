1a2eb8f3ba17bdc707a4996660515e17157238d4
Patrick <ptemy5@gmail.com>
2024-03-28 17:31:38 -0300

    avaliacao-formadora-01 / mini-portifolio

--- avaliacao-formadora-01/capacitor.config.ts
+++ avaliacao-formadora-01/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'ionic',
+  appName: 'avaliacao-formadora-01',
   webDir: 'www',
   server: {
     androidScheme: 'https'
